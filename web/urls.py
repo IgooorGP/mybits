8818b845ec46dde4c25f641ee149eee73b9f48e0
IgooorGP <igor.feq@gmail.com>
2018-05-15 00:18:04 -0300

    added static files and created mybits logo

--- web/urls.py
+++ web/urls.py
@@ -3,11 +3,16 @@ Views for the web app.
 """
 from web import views
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path('', views.index, name= 'index')
-] + static(settings.STATIC_URL)
+    url(r'^$', views.index, name='index')
+] 
+
+if settings.DEBUG:
+    # adds /static/ for all files in web/static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
