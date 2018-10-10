3b2a43cfcd24706ad102cacf9851008ca8895508
IgooorGP <igor.feq@gmail.com>
2018-10-10 09:37:05 -0300

    css fixes. changed scroll bar colors

--- backbits/frontbits/src/app/main-display/about-me/about-me.component.ts
+++ backbits/frontbits/src/app/main-display/about-me/about-me.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss', '../ask-the-bot/ask-the-bot.component.scss']
+  styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
 
