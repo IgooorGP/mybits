cffae33d1273c33c97cf711290f68a788c97a02a
Ígor Grillo Peternella <igor.feq@gmail.com>
2018-10-23 22:53:51 -0300

    added mysql as the backend database

---
+++ backbits/bot.py
@@ -0,0 +1,12 @@
+"""
+Module with some basic intelligence for the chat bot
+in order to answer some questions about me.
+"""
+
+
+class ChatBot:
+    """
+    Class that represents the bot intelligence.
+    """
+
+    pass
